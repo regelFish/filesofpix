241b4d36368d49fd36903f172c1cf5766ec1dd95
regelFish <168052544+regelFish@users.noreply.github.com>
2025-01-20 23:05:33 -0500

    Add files via upload

---
+++ readaline.c
@@ -0,0 +1,22 @@
+#include "readaline.h"
+
+
+size_t readaline(FILE *inputfd, char **datapp) 
+{
+    char buffer[1001];
+
+    fgets(buffer, sizeof(buffer), inputfd);
+
+    // If the last character of buffer is not '\n', that means the line was too
+    // long.
+
+    datapp = buffer;
+    
+    size_t length = 0;
+
+    while (buffer[length] != '\0') {
+        length++;
+    }
+
+    return length;
+}

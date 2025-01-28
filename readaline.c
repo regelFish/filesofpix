01814df6ad3ace7160bae841cbce8bf32c06947e
Lawer Nyako <86574038+lawaddle@users.noreply.github.com>
2025-01-28 01:29:23 -0500

    This is what I have made for readaline. DO NOT MERGE THIS BRANCH WITH MAIN UNTIL AFTER TESTING.

--- readaline.c
+++ readaline.c
@@ -1,22 +1,66 @@
 #include "readaline.h"
-
+#include <stdio.h>
+int numChars = 0;
+int capacity = 2;
 
 size_t readaline(FILE *inputfd, char **datapp) 
 {
-    char buffer[1001];
+        char *buff;
+        buff = malloc(sizeof(char)*capacity);
+        mallocCheck(buff, sizeof(char)*capacity);
+        free(buff);
 
-    fgets(buffer, sizeof(buffer), inputfd);
+        while ((ch = fgetc(inputfd))!= EOF) {
+                buff[numChars] = ch;
+                numChars++;
+                expand(&buff);
+        }
+        // char buffer[1001];
 
-    // If the last character of buffer is not '\n', that means the line was too
-    // long.
+        // fgets(buffer, sizeof(buffer), inputfd);
 
-    datapp = buffer;
-    
-    size_t length = 0;
+        // // If the last character of buffer is not '\n', that means the line was too
+        // // long.
 
-    while (buffer[length] != '\0') {
-        length++;
-    }
+        // datapp = buff;
 
-    return length;
+        // size_t length = 0;
+
+        // while (buffer[length] != '\0') {
+        //         length++;
+        // }
+
+        if (numChars == 0) {
+                datapp = NULL;
+        } else {
+                datapp = &buff;
+        }
+
+        return numChars;
+}
+
+bool mallocCheck(char *p, int SIZE) 
+{
+        if (p == NULL) {
+                fprintf(stderr, "Failed to malloc %d bytes of memory\n", SIZE);
+                return EXIT_FAILURE;
+        //what is a checked runtime error
+        } 
+}
+
+void expand(char **buffer) 
+{
+        if(numItems == capacity) {
+                capacity = capacity*2 + 2;
+                char *temp = malloc(sizeof(char)*capacity);
+                mallocCheck(temp, sizeof(char)*capacity);
+
+                for (int i = 0; i < numItems; i++) {
+                        temp[i] = data[i];
+                }
+                free(*buffer);
+                buffer = &temp;
+        }        
+
+
 }

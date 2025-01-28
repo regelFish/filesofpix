d5e04c3fe4cc8aa4fe6c3f0522dbb24453e36c52
regelFish <168052544+regelFish@users.noreply.github.com>
2025-01-28 13:29:31 -0500

    Update readaline.c
    Formatting fix

--- readaline.c
+++ readaline.c
@@ -3,20 +3,22 @@
 
 size_t readaline(FILE *inputfd, char **datapp) 
 {
-    char buffer[1001];
+        char buffer[1001];
 
-    fgets(buffer, sizeof(buffer), inputfd);
+        fgets(buffer, sizeof(buffer), inputfd);
 
-    // If the last character of buffer is not '\n', that means the line was too
-    // long.
+        /*  
+        If the last character of buffer is not '\n', that means the line was too
+        long. 
+        */
 
-    datapp = buffer;
+        datapp = buffer;
     
-    size_t length = 0;
+        size_t length = 0;
 
-    while (buffer[length] != '\0') {
-        length++;
-    }
+        while (buffer[length] != '\0') {
+            length++;
+        }
 
-    return length;
+        return length;
 }

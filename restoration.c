5b70b143c06e0590b7fae972e9be34faeeec257b
Lawer Nyako <86574038+lawaddle@users.noreply.github.com>
2025-01-28 02:15:49 -0500

    notes added and restoration added because i wanted to make sure the compiler did what i thought it did

---
+++ restoration.c
@@ -0,0 +1,12 @@
+#include "readaline.h"
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(int argc, char *argv[])
+{
+        (void) argc;
+        (void) argv;
+
+        printf("hello, world");
+        return EXIT_SUCCESS;
+}
